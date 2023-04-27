c728c71debd874b4d68fec7c0632505ddbcb5709
Patricio Gallardo Silva <patricio.gallardo.s@gmail.com>
2023-04-26 22:31:41 -0400

    quinto commit se agrega scrool top

--- assets/js/viajes.js
+++ assets/js/viajes.js
@@ -8,3 +8,27 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
 $( "#enviar" ).click(function() {
     alert( "El correo fue enviado correctamente." );
 });
+
+$("a").click(function() {
+    console.log(this.hash);
+
+    if (this.hash !== "") {
+        event.preventDefault();
+
+        console.log(this.hash);
+
+        var hash = this.hash;
+
+        console.log(hash);
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 600, function() {
+
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+        });
+
+    }
+
+});
